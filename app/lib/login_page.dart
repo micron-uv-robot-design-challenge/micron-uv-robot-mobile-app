3d5a37db2f006e9d2bb33ca041058480fe060b1c
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-07-03 12:03:02 +0800

    Add Container and SingleChildScrollView to fix bottom overflow

--- app/lib/login_page.dart
+++ app/lib/login_page.dart
@@ -10,25 +10,29 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[700],
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 100.0),
-          Text(
-            'Micron UV robot',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
-            ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100.0),
+              Text(
+                'Micron UV robot',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+              SizedBox(height: 280.0),
+              // Text("Test"),
+              Username(),
+              Password(),
+              SizedBox(height: 10.0),
+              // Text("Test"),
+              LoginButton(),
+            ],
           ),
-          SizedBox(height: 280.0),
-          // Text("Test"),
-          Username(),
-          Password(),
-          SizedBox(height: 10.0),
-          // Text("Test"),
-          LoginButton(),
-        ],
+        ),
       ),
     );
   }
