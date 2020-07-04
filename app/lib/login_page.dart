2ea28cb1c806d89433cb56f06c751c334768ef4c
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-07-04 16:21:44 +0800

    Combine all widget classes into the login page class

--- app/lib/login_page.dart
+++ app/lib/login_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './username.dart';
-import './password.dart';
-import './login_button.dart';
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -28,10 +24,101 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               SizedBox(height: 250.0),
-              Username(),
-              Password(),
+
+              // Username
+              Padding(
+                padding: new EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF6CA8F1),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6.0,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  height: 60.0,
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.account_box,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter your username',
+                      hintStyle: TextStyle(
+                        color: Colors.white54,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Password
+              Padding(
+                padding: new EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF6CA8F1),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6.0,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  height: 60.0,
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(
+                        color: Colors.white54,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
               SizedBox(height: 28.0),
-              LoginButton(),
+
+              // Login button
+              RaisedButton(
+                padding: const EdgeInsets.all(10.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text(
+                  'Administrator Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                color: Color(0xFF6CA8F1),
+                onPressed: () {},
+              ),
             ],
           ),
         ),
